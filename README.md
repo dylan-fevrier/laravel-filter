608d6b87378b70893ed738fb0cd2ce9a706b9818
Dylan Février <dev@dylanfevrier.com>
2020-01-18 13:44:09 +0100

    Feat : Add Filter

---
+++ README.md
@@ -0,0 +1,23 @@
+# Laravel Filter
+
+## Installation
+
+To install Laravel Filter, you need use `composer`.
+
+''''
+composer require dfevrier/laravel-filter
+''''
+
+## Import
+
+In your controller, add the import line.
+
+''''
+class ExampleController extends Controller
+{
+
+    use Dfevrier\LaravelFilter\Filter;
+}
+''''
+
+## Usage
--- src/Filter.php
+++ src/Filter.php
@@ -2,10 +2,20 @@
 
 namespace Dfevrier\LaravelFilter;
 
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+
 trait Filter
 {
 
-    public function filter($model, $columns)
+    /**
+     * [filter description]
+     * @param  Model   $model   [description]
+     * @param  array   $columns [description]
+     * @param  Request $request [description]
+     * @return Model            [description]
+     */
+    public function filter(Model $model, array $columns, Request $request)
     {
         foreach ($columns as $key => $value) {
             if (is_array($value)) {
@@ -16,7 +26,28 @@ trait Filter
             if (!$request->has($columnName) || $request->get($columnName) === NULL) {
                 continue;
             }
-            
+            if (is_array($value) && isset($value['operator'])) {
+                $model = $this->addFilter($model, $columnName, $request->get($columnName), $value['operator']);
+            } else {
+                $model = $this->addFilter($model, $columnName, $request->get($columnName));
+            }
         }
+        return $model;
+    }
+
+    /**
+     * Add a filter to query.
+     * @param  Model  $model    [description]
+     * @param  string $column   [description]
+     * @param  string $search   [description]
+     * @param  string $operator [description]
+     */
+    private function addFilter(Model $model, string $column, string $search, ?string $operator = '=')
+    {
+        if ($operator == 'like') {
+            $search = '%' . $search . '%';
+        }
+        $model = $model->where($column, $operator, $search);
+        return $model;
     }
 }
